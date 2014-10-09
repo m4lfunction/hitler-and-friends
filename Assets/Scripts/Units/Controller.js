cf51f25cb54b7edffc6910a30e2e2c8db02e0906
Daniel Pleske <daniel.pleske@gmail.com>
2014-10-10 01:02:30 +0200

    Added a second testscene

---
+++ Assets/Scripts/Units/Controller.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+var walkSpeed : float = 10;
+private var speed : float;
+
+var nozzle : GameObject;
+
+function Start () {
+	speed = walkSpeed;
+}
+
+function Update(){
+
+}
+
+function FixedUpdate () {
+	MoveCharacter();
+	nozzle.transform.Translate(Vector3.forward * Time.deltaTime * speed * Input.GetAxis("AimVertical"));
+	nozzle.transform.Translate(Vector3.right * Time.deltaTime * speed * Input.GetAxis("AimHorizontal"));
+}
+
+function MoveCharacter(){
+	transform.Translate(Vector3.forward * Time.deltaTime * speed * Input.GetAxis("Vertical"));
+	transform.Translate(Vector3.right * Time.deltaTime * speed * Input.GetAxis("Horizontal"));
+}
--- Assets/Scripts/Weapons/GunControl.js
+++ Assets/Scripts/Weapons/GunControl.js
@@ -9,20 +9,21 @@ function Start () {
 }
 
 function Update () {
-	var ray : Ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-	var hit : RaycastHit;
-	var playerPlane = new Plane(Vector3.up, transform.position);
+//	var ray : Ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+//	var hit : RaycastHit;
+//	var playerPlane = new Plane(Vector3.up, transform.position);
 	
 	if (Input.GetButton("Fire1")){
 	  
-		var bullet = Instantiate(bulletBase, nozzle.position, nozzle.rotation);
+		var bullet = Instantiate(bulletBase, nozzle.position, transform.rotation);
 		bullet.rigidbody.AddForce (Vector3.forward * shootForce);
-		if (Physics.Raycast(ray, hit)){
-      var dir = hit.point - bullet.transform.position;
-      bullet.rigidbody.AddForce(dir.normalized * shootForce, ForceMode.Impulse);
+//		if (Physics.Raycast(ray, hit)){
+//      var dir = hit.point - bullet.transform.position;
+      //
+      bullet.rigidbody.AddForce(nozzle.position * shootForce, ForceMode.Impulse);
       }
 	}
 	
 	
 	
-}
+//}
